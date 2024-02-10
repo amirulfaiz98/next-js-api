b0e7cd46c4c6f81d996b73131c3ef200da148283
Amirul Faiz <faiz@baituljannah.com>
2024-02-10 12:47:04 +0800

    Parsing The Incoming Request & Executing Server-side Code

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,4 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
 function handler(req, res) {
+    if (req.method === 'POST') {
+        const email = req.body.email;
+        const feedbackText = req.body.text;
+
+        const newFeedback = {
+            id: new Date().toISOString(),
+            email : email,
+            text : feedbackText
+        }
+
+        // store in database
+        const filepath = path.join(process.cwd(), 'data', 'feedback.json');
+        const fileData = fs.readFileSync(filepath);
+        const data = JSON.parse(fileData);
+        data.push(newFeedback);
+        fs.watchFileSync(filepath, JSON.stringify(data));
+
+        res.status(201).json({ message: 'Success', feedback: newFeedback })
+    }
     res.status(200).json({ message: 'This works!' })
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
+import { useRef } from 'react';
+
 function HomePage() {
+  const userInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = userInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={userInputRef}/>
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Email Address</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
