f23836b3952b352173b108e6a9e9310ec021c5e1
Amirul Faiz <faiz@baituljannah.com>
2024-02-13 11:27:55 +0800

    Creating & Using Dynamic API Routes

---
+++ pages/api/[feedbackId].js
@@ -0,0 +1,12 @@
+import { buildFeedbackPath, extractFeedback } from "./feedback";
+
+function handler(req, res) {
+  const feedbackId = req.query.feedbackId;
+  const filepath = buildFeedbackPath();
+  const feedbackData = extractFeedback(filepath);
+
+  const selectedFeedback = feedbackData.find((item) => item.id === feedbackId);
+  res.status(200).json({ feedback: selectedFeedback });
+}
+
+export default handler;
