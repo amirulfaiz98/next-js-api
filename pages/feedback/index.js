3da36c3e7fb0be24097477a3182e0ba5f5b081ae
Amirul Faiz <faiz@baituljannah.com>
2024-02-13 11:31:41 +0800

    Exploring Different Ways Of Structuring API Route

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -5,7 +5,7 @@ import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 function FeedbackPage(props) {
   const [feedbackData, setFeedbackData] = useState();
   function loadFeedbackHandler(id) {
-    fetch(`/api/${id}`)
+    fetch(`/api/feedback/${id}`)
       .then((response) => response.json())
       .then((data) => {
         setFeedbackData(data.feedback);
